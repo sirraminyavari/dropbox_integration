a195615c892751a76fbba7af4e82defffe68f3f8
DCmanager <DCmanager@nle.com>
2021-05-21 20:04:03 +0430

    implement fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import DropboxMain from './components/dropbox'
-import { useEffect, useState, useCallback } from 'react';
-import { parseQueryString } from './components/utils'
+import { useEffect} from 'react';
 
 function App() {
 
   useEffect(() => {
-    const accessToken = parseQueryString(window.location.hash).access_token;
-
-    if (window.opener && accessToken) {
-      window.opener.postMessage(accessToken);
+    
+    const hash = window.location.hash;
+    const search = window.location.search;
+  
+    if (window.opener) {
+      const data = {hash, search}
+      window.opener.postMessage(data);
       window.close();
     }
   
--- src/components/dropbox.jsx
+++ src/components/dropbox.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Dropbox} from 'dropbox';
+import { parseQueryString } from './utils'
+
 
 const STORAGE_KEY = 'pwnzolgprg7hngfkjscg59vd'
 const CLIENT_ID = `gguc9kwsbgr920c`;
@@ -21,32 +23,61 @@ const dbxInitialState = () => {
 const DropboxMain = () => {
 
   const [dbxData, setDbxData] = useState(() => dbxInitialState());
+  const [dbx, setDbx] = useState(null);
 
   useEffect(() => {
     window.addEventListener('message', e => {
-
-      // Do we trust the sender of this message? (might be
-      // different from what we originally opened, for example).
-      if (e.origin !== 'http://localhost:3000') {
-        return;
+      const data = e.data;
+      if (data) {
+        const { hash, search } = e.data;
+        // Do we trust the sender of this message? (might be
+        // different from what we originally opened, for example).
+        if (e.origin !== 'http://localhost:3000') {
+          return;
+        }
+        const accessToken = parseQueryString(hash).access_token;
+        if(accessToken) {
+          window.localStorage.setItem(STORAGE_KEY, JSON.stringify({ loggedIn: true, accessToken}));
+          setDbxData({ loggedIn: true, accessToken});
+          setDbx(new Dropbox({ accessToken}));
+        }
       }
-      window.localStorage.setItem(STORAGE_KEY, JSON.stringify({ loggedIn: true, accessToken: e.data}));
-      setDbxData({ loggedIn: true, accessToken: e.data})
-    });
+    }, false);
   });
+
+  useEffect(() => {
+    if(dbxData.loggedIn) {
+      setDbx(new Dropbox({ accessToken: dbxData.accessToken , fetch}));
+    }
+  }, [dbxData]);
+
+  useEffect(() => {
+    if (dbx !== null) {
+      initializeFiles();
+    }
+  }, [dbx]);
+
+  const initializeFiles = () => {
+    console.log(dbx);
+    dbx.filesListFolder({
+      path: '',
+      limit: 2
+    }).then(res => { console.log(res)}).catch(e => console.log(e));
+  }
 
   const login = async() => {
 
     const dbx = new Dropbox({ clientId: CLIENT_ID });
     const url = await dbx.auth.getAuthenticationUrl(REDIRECT_URI);
-    const features ='toolbar=no, menubar=no, width=600, height=700, top=100, left=100';
+    const features ='toolbar=no, menubar=no, width=600, height=700, top=200, left=200';
 
-    window.open(url, 'login', '_blank', features);
+    window.open(url, 'login', '_blank', features, true);
   }
 
  
   const logout = () => {
-
+      window.localStorage.removeItem(STORAGE_KEY);
+      setDbxData({ loggedIn: false, undefined})
   }
 
   return (
@@ -54,6 +85,7 @@ const DropboxMain = () => {
       <div>Drop Box</div>
       { !dbxData.loggedIn &&  <button onClick={() => login()}>login</button>}
       { dbxData.loggedIn &&  <div >Your Logged In</div>}
+      { dbxData.loggedIn &&  <button onClick={() => logout()}>logout</button>}
     </div>
   )
 }
